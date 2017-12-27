4fe7699ce07cf0d5db709cb03de2ab7c935adf14
Rasim Sadrutdinov <ras2kas@mail.ru>
2017-12-27 19:45:48 +0300

    1

--- script.js
+++ script.js
@@ -1,204 +1,110 @@
-var nameFrends = document.querySelector('#name'),
-	nameList = document.querySelector('#name2'),
-	friends = [];
+var array = [];
+let myMap;
+let clusterer;
+new Promise(resolve => ymaps.ready(resolve))
+    .then(() => {
+        myMap = new ymaps.Map('map', {
+            center: [55.753994, 37.622093],
+            zoom: 9
+        }, {
+            searchControlProvider: 'yandex#search'
+        });
 
-VK.init({
-	apiId: 6301007
-});
+        clusterer = new ymaps.Clusterer({
+            preset: 'islands#invertedVioletClusterIcons',
+            clusterDisableClickZoom: true,
+            openBalloonOnClick: true
+        });
 
-function auth() {
-	return new Promise((resolve, reject) => {
-		VK.Auth.login(data => {
-			if (data.session) {
-				resolve();
-			} else {
-				reject(new Error('Не удалось авторизоваться'));
-			}
-		}, 2);
-	});
+        myMap.geoObjects.add(clusterer);
+
+        myMap.events.add('click', function(e) {
+            var content = document.getElementById('content');
+            content.innerHTML = '';
+            var coords = e.get('coords');
+            myPlacemark = new ymaps.Placemark(coords, {
+            preset: 'islands#violetDotIconWithCaption',
+            iconColor: '#0095b6',
+            }, {
+            balloonCloseButton: true,
+            hideIconOnBalloonOpen: false
+            });
+            clusterer.add(myPlacemark);
+            container.style.display = 'block';
+            let top = e.get('position')[1] + 'px',
+                left = e.get('position')[0] + 'px';
+            container.style.top = e.get('position')[1] + 'px';
+            container.style.left = e.get('position')[0] + 'px';
+            getAddress(coords);
+        });
+
+        var container = document.getElementById('container');
+
+        container.addEventListener('click', function(e) {
+            if (e.target.tagName == 'BUTTON') {
+                let obj = {},
+                    userName = document.getElementById('name'),
+                    userPlace = document.getElementById('place'),
+                    userText = document.getElementById('text'),
+                    adr = adres.innerHTML;
+
+                obj.userAddress = adres.innerHTML,
+                obj.userName = userName.value,
+                obj.userPlace = userPlace.value,
+                obj.userText = userText.value;
+
+                document.getElementById('name').value = '';
+                document.getElementById('place').value = '';
+                document.getElementById('text').value = '';
+
+                array.push(obj);
+                print(array, adr);
+            }
+            if (e.target.id == 'close') {
+                container.style.display = 'none';
+            }
+        })
+
+
+
+    })
+
+function print(arr, address) {
+    let arr2 = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].userAddress == address) {
+            arr2.push(arr[i]);
+        }
+    }
+    let template = document.querySelector('#user-template').textContent,
+        render = Handlebars.compile(template),
+        result = document.getElementById('content');
+    result.innerHTML = '';
+    html = render({
+        arr2: arr2
+    });
+    result.innerHTML = html;
 }
 
-function callAPI(method, params) {
-	params.v = '5.69';
 
-	return new Promise((resolve, reject) => {
-		VK.api(method, params, (data) => {
-			if (data.error) {
-				reject(data.error);
-			} else {
-				resolve(data.response);
-			}
-		});
-	})
+function createPlacemark(coords) {
+    return new ymaps.Placemark(coords, {
+        preset: 'islands#violetDotIconWithCaption',
+        iconColor: '#0095b6',
+    }, {
+        balloonCloseButton: true,
+        hideIconOnBalloonOpen: false
+    });
+
 }
 
-function saveLS(friends, list) {
-	localStorage.removeItem('friends');
-	localStorage.removeItem('list');
-	localStorage.friends = JSON.stringify(friends);
-	localStorage.list = JSON.stringify(list);
+
+function getAddress(coord) {
+    var myGeocoder = ymaps.geocode(coord);
+    myGeocoder.then(function(res) {
+            var nearest = res.geoObjects.get(0);
+            var name = nearest.getAddressLine();
+            adres.innerHTML = name;
+        }
+    );
 }
-
-function PrintFriends(friend, list) {
-	let friendsList = document.getElementById('friendsList'),
-		result = document.getElementById('result'),
-		template = document.querySelector('#user-template').textContent,
-		template2 = document.querySelector('#user-template2').textContent,
-		render = Handlebars.compile(template),
-		render2 = Handlebars.compile(template2),
-		html = render(friend),
-		html2 = render2(list);
-	result.innerHTML = html;
-	friendsList.innerHTML = html2;
-
-	for (let b = 0; b < friend.items.length; b++) {
-		let filter = document.getElementById(friend.items[b].id);
-		filter.style.display = 'block';
-		let str = friend.items[b].first_name + ' ' + friend.items[b].last_name;
-
-		if (!isMatching(str, nameFrends.value)) {
-			filter.style.display = 'none';
-		}
-	}
-
-	for (let b = 0; b < list.items.length; b++) {
-		let filter = document.getElementById(list.items[b].id);
-		filter.style.display = 'block';
-		let str2 = list.items[b].first_name + ' ' + list.items[b].last_name;
-		if (!isMatching(str2, nameList.value)) {
-			filter.style.display = 'none';
-		}
-	}
-}
-
-function isMatching(full, chunk) {
-	if ((full.toLowerCase().indexOf(chunk.toLowerCase())) !== -1) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-(async () => {
-	try {
-		await auth();
-		if ((localStorage.friends) && (localStorage.list.length)) {
-			friends = JSON.parse(localStorage.friends);
-			list2 = JSON.parse(localStorage.list);
-			var list = list2.items;
-			PrintFriends(friends, list2)
-
-		} else {
-			friends = await callAPI('friends.get', {
-				fields: 'city,country, photo_100'
-			});
-			var template = document.querySelector('#user-template').textContent,
-				render = Handlebars.compile(template),
-				html = render(friends),
-				results = document.querySelector('#result'),
-				list = [],
-				list2 = {};
-				results.innerHTML = html;
-			
-		}
-		var friendsList = document.querySelector('#friendsList'),
-			friend = document.getElementById("myfriends"),
-			myList = document.getElementById("list"),
-			main = document.getElementById("main"),
-			saiv = document.getElementById("save"),
-			close = document.getElementById("close");
-		result.addEventListener('click', function(e) {
-			if (e.target.className == 'plus') {
-				for (var i = 0; i < friends.items.length; i++) {
-					if (friends.items[i].id == e.target.name) {
-						list.push(friends.items[i]);
-						friends.items.splice(i, 1);
-					}
-				}
-				list2['items'] = list;
-				PrintFriends(friends, list2);
-			}
-		})
-		friendsList.addEventListener('click', function(e) {
-			if (e.target.className == 'remove') {
-				for (var a = 0; a < list2.items.length; a++) {
-					if (list2.items[a].id == e.target.name) {
-						friends.items.push(list2.items[a]);
-						list2.items.splice(a, 1);
-						PrintFriends(friends, list2);
-						saveLS(friends, list2);
-					}
-				}
-			}
-		})
-		nameFrends.addEventListener('keyup', function() {
-			PrintFriends(friends, list2);
-		})
-		nameList.addEventListener('keyup', function() {
-			PrintFriends(friends, list2);
-		})
-		saiv.addEventListener('click', function() {
-			saveLS(friends, list2)
-		})
-		
-		close.addEventListener('click', function() {
-			let content = document.getElementById("container");
-			content.style.display = 'none';
-		})
-		friend.addEventListener('mousedown', function(e) {
-			console.log(e.target.className);
-			if ((e.target.tagName == 'INPUT') || (e.target.className == 'friends style-3') || (e.target.className == 'content left')) {
-				return;
-			}
-			if (e.target.tagName == 'DIV') {
-				var block = e.target;
-			} else {
-				var block = e.target.parentNode;
-			}
-			var coords = getCoords(block),
-				shiftX = e.pageX - coords.left,
-				shiftY = e.pageY - coords.top;
-
-			function move(e) {
-				block.style.left = e.pageX - shiftX + 'px';
-				block.style.top = e.pageY - shiftY + 'px';
-			}
-
-			function getCoords(elem) {
-				var box = elem.getBoundingClientRect();
-				return {
-					top: box.top + pageYOffset,
-					left: box.left + pageXOffset
-				};
-			}
-
-			block.style.position = 'absolute';
-			block.style.zIndex = 1000;
-			move(e);
-
-			document.onmousemove = function(e) {
-				move(e);
-			}
-
-			block.onmouseup = function(e) {
-				let BlockCoord = block.getBoundingClientRect();
-				let ListCoord = myList.getBoundingClientRect();
-				if ((BlockCoord.top > ListCoord.top - 10) && (BlockCoord.bottom < ListCoord.bottom + 10) && (BlockCoord.left > ListCoord.left - 10) && (BlockCoord.right < ListCoord.right + 10)) {
-					console.log(block.id);
-					for (var i = 0; i < friends.items.length; i++) {
-						if (friends.items[i].id == block.id) {
-							list.push(friends.items[i]);
-							list2['items'] = list;
-							friends.items.splice(i, 1);
-						}
-					}
-					PrintFriends(friends, list2);
-				} else {
-					block.style.position = '';
-				}
-			}
-		})
-	} catch (e) {
-		console.error(e);
-	}
-})();
